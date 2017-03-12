ebb2b23c00bb247b3323428ae19ce87d0005cafb
Ultrae <mathieu.ghirlanda@epita.fr>
2017-03-12 05:56:31 +0000

    Delete Admin.php

--- Admin.php
+++
@@ -1,6 +0,0 @@
-<?php
-include("Connexion.php");
-$codeSQL = "INSERT INTO `sharemywash`.`USERS` (`id`, `Name`, `Email`, `Location`, `CurentLaundry`, `Dryer`, `WMachine`) VALUES (NULL, '$name', '$email', '$location', '$current', '$dryer', '$wMachine');";
-$req = mysql_query($codeSQL);
-echo "ok";
-?>
