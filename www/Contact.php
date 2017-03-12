cfb2ec0e444009da908329abe58248bc28457a47
Ultrae <mathieu.ghirlanda@epita.fr>
2017-03-12 08:40:46 +0000

    Add files via upload

---
+++ www/Contact.php
@@ -0,0 +1,101 @@
+<!DOCTYPE HTML>
+<?php
+header( 'content-type: text/html; charset=utf-8' );
+?>
+<html>
+<head>
+    <title>SMW</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/skel.min.js"></script>
+    <script src="js/skel-layers.min.js"></script>
+    <script src="js/init.js"></script>
+    <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v0.33.1/mapbox-gl.js'></script>
+    <noscript>
+        <link rel="stylesheet" href="css/skel.css" />
+        <link rel="stylesheet" href="css/style.css" />
+        <link rel="stylesheet" href="css/style-xlarge.css" />
+        <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.33.1/mapbox-gl.css' rel='stylesheet' />
+    </noscript>
+</head>
+<body class="landing">
+
+    <!-- Header -->
+    <header id="header" class="alt">
+
+        <h1>
+            <strong>
+                <a href="index.php">Share My Wash</a>
+            </strong>
+        </h1>
+        <?php include"Table.php"?>
+    </header>
+
+   
+    <!-- One -->
+    <section id="one" class="wrapper style1">
+        <div class="container 75%">
+            <div class="row 200%">
+                <div class="6u 12u$(medium)">
+                    <header class="major">
+                        <h2>What is it ?</h2>
+                        <p>Wash Revolution Incoming</p>
+                    </header>
+                </div>
+
+
+
+
+                <div></div>
+                <p>
+                    You are a student. You have to do your laundry. But your washing machine is not filled entirely? Do not worry about this anymore!
+                    <br />
+                    With
+                    <b>Share My Wash</b>, you are now abble to share your washing machine! Like this you can save a washing machine for other, but you can also share the fees.
+                    <br />
+                    Moreover, it is a way to socialize with others and maybe discover some interresting people!
+                    <br />
+                    You are a business man. You have a coffee spot on your sleeve and you have to clean it before your meeting? However, you have only this clothe to wash and you think that it is a waste of water to use it only for one thing? Share My Wash is there to help you and find someone with whom you can wash your sleeve!
+                </p>
+
+            </div>
+        </div>
+    </section>
+    <!-- Two -->
+    <section id="two" class="wrapper style2">
+ 
+    
+        <div class="container 75%">
+            <div class="row 200%">
+                <div class="6u 12u$(medium)">
+                    <header class="major">
+                        <h2>What is it ?</h2>
+                        <p>Wash Revolution Incoming</p>
+                    </header>
+                </div>
+
+
+
+
+                <div></div>
+                <p>
+                    You are a student. You have to do your laundry. But your washing machine is not filled entirely? Do not worry about this anymore!
+                    <br />
+                    With
+                    <b>Share My Wash</b>, you are now abble to share your washing machine! Like this you can save a washing machine for other, but you can also share the fees.
+                    <br />
+                    Moreover, it is a way to socialize with others and maybe discover some interresting people!
+                    <br />
+                    You are a business man. You have a coffee spot on your sleeve and you have to clean it before your meeting? However, you have only this clothe to wash and you think that it is a waste of water to use it only for one thing? Share My Wash is there to help you and find someone with whom you can wash your sleeve!
+                </p>
+
+            </div>
+        </div>
+    </section>
+    <!-- Footer -->
+    <?php include"Foot.php" ?>
+</body>
+</html>
