0f6bfa96b069f4bc0dddda7b15f495563b6746a3
Ultrae <mathieu.ghirlanda@epita.fr>
2017-03-12 08:41:29 +0000

    Add files via upload

--- www/index.php
+++ www/index.php
@@ -122,23 +122,45 @@ header( 'content-type: text/html; charset=utf-8' );
     </section>
     <!-- Two -->
     <section id="two" class="wrapper style2">
+        <style>
+            body {
+                margin: 0;
+                padding: 0;
+            }
+
+            #map {
+                position: static;
+                top: 51%;
+                bottom: 100%;
+                height: 35%;
+                width: 40%;
+            }
+        </style>
         <div class="container 75%">
             <div class="row 200%">
                 <div class="6u 12u$(medium)">
                     <header class="major">
                         <h2>What is it ?</h2>
-                        <p>A good way to wash his laundry</p>
+                        <p>Wash Revolution Incoming</p>
                     </header>
-                    </div>
                 </div>
-                    <div class="6u$ 12u$(medium)">
-                        <p>
-                            blablablablablabla
-                        </p>
+        
 
-                        </div>
+
+
+                    <div>
+          
+                    </div>
+                    <p>
+                        You are a student. You have to do your laundry. But your washing machine is not filled entirely? Do not worry about this anymore! <br />
+                        With <b>Share My Wash</b>, you are now abble to share your washing machine! Like this you can save a washing machine for other, but you can also share the fees.<br />
+                        Moreover, it is a way to socialize with others and maybe discover some interresting people! <br />
+                        You are a business man. You have a coffee spot on your sleeve and you have to clean it before your meeting? However, you have only this clothe to wash and you think that it is a waste of water to use it only for one thing? Share My Wash is there to help you and find someone with whom you can wash your sleeve!
+                    </p>
+                
             </div>
-        </section>
+        </div>
+    </section>
                         <!-- Footer -->
                         <?php include"Foot.php" ?>
 </body>
